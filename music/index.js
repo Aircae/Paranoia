6b065dad2b0d5324ad990d39a46971f6f81fa333
Aircae <f2180189549@163.com>
2025-03-11 15:35:06 +0800

    fix

--- music/index.js
+++ music/index.js
@@ -2,7 +2,6 @@ function toggleSidebar() {
 	document.querySelector('.sidebar').classList.toggle('active');
 }
 
-// 点击外部区域关闭侧边栏
 document.addEventListener('click', function(event) {
 	const sidebar = document.querySelector('.sidebar');
 	const toggleBtn = document.querySelector('.toggle-btn');
